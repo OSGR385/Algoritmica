10bc7a21fd2174311cfa4c83ca3a5e59dfbbd02d
Omar S. García R. <shadid140806@gmail.com>
2024-09-09 01:01:13 -0700

    Ac2

---
+++ Comentarios.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+
+main(){
+
+	printf("Hola Mundo");
+	printf("\nHola Mundo"); //Salto de linea
+	printf("\nX   \nY"); //Orientar Texto
+	printf("\n\n Hola Mundo"); // Alerta
+   printf("\n\n\"Hola Mundo\"");
+
+   getch();
+}
