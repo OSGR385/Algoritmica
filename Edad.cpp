9c7a92b6f5598937d6ae44a7bd2f101dedb268c7
Omar S. García R. <shadid140806@gmail.com>
2024-09-20 13:44:33 -0700

    Edad.cpp

--- Edad.cpp
+++ Edad.cpp
@@ -7,9 +7,9 @@ int Dia,Mes,Year,Edad;
 main(){
   printf("Escribe dia de nacimiento\n");
   scanf("%d",&Dia);
-  printf("Escribe dia de nacimiento\n");
+  printf("Escribe Mes de nacimiento\n");
   scanf("%d",&Mes);
-  printf("Escribe dia de nacimiento\n");
+  printf("Escribe Año de nacimiento\n");
   scanf("%d",&Year);
   Edad = 2024-Year;
   printf("\nTu edad es de %d :\n",Edad);
